529f22f1ffc45bab04ca7b338bc7e5842d9f8285
Best-Cavitt-Robotics-Team <bestcavittvexteam@gmail.com>
2026-02-02 20:34:52 -0800

    Add files via upload
    Added a move forward

---
+++ robot-config.cpp
@@ -0,0 +1,43 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen.
+brain  Brain;
+
+//The motor constructor takes motors as (port, ratio, reversed), so for example
+//motor LeftFront = motor(PORT1, ratio6_1, false);
+
+//Add your devices below, and don't forget to do the same in robot-config.h:
+controller Controller = controller(primary);
+
+motor RightFront = motor(PORT10, ratio6_1, false);
+motor RightMiddle = motor(PORT9, ratio6_1, true);
+motor RightBack = motor(PORT8, ratio6_1, false);// in 9 right now
+
+motor LeftFront = motor(PORT7, ratio6_1, true);
+motor LeftMiddle = motor(PORT6, ratio6_1, false);
+motor LeftBack = motor(PORT5, ratio6_1, true); 
+
+motor IntakeBottom = motor(PORT1, ratio18_1, false);
+motor IntakeTop = motor(PORT2, ratio18_1, false);
+
+pneumatics Scraper1(Brain.ThreeWirePort.H);
+pneumatics Scraper2(Brain.ThreeWirePort.G);
+pneumatics descore(Brain.ThreeWirePort.F);
+pneumatics middleGoal(Brain.ThreeWirePort.E);
+
+led WheelPiston(Brain.ThreeWirePort.C);
+
+inertial Inertial = inertial(PORT19);
+
+rotation VerticalOdom = rotation(PORT4, true);
+rotation HorizontalOdom = rotation(PORT3, false);
+
+
+
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
---
+++ robot-config.h
@@ -0,0 +1,29 @@
+using namespace vex;
+
+extern brain Brain;
+
+//To set up a motor called LeftFront here, you'd use
+//extern motor LeftFront;
+
+//Add your devices below, and don't forget to do the same in robot-config.cpp:
+extern motor IntakeBottom;
+extern motor IntakeTop;
+extern motor LeftFront;
+extern motor LeftMiddle;
+extern motor LeftBack;
+extern motor RightFront;
+extern motor RightMiddle;
+extern motor RightBack;
+
+extern rotation horizontalRotational;
+extern rotation verticalRotational;
+
+extern inertial Inertial;
+
+extern pneumatics Scraper1;
+extern pneumatics Scraper2;
+extern pneumatics descore;
+extern pneumatics middleGoal;
+
+
+void  vexcodeInit( void );
